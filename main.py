7eebd8c448fb67f32c7fcc333a0a105de2c544e9
WrongDay <43218868+WrongDay@users.noreply.github.com>
2018-10-14 19:45:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -195,8 +195,8 @@ async def ping(ctx):
 async def credits(ctx):
     embed = discord.embed(color = 0x00ff00)
     embed.set_author(name = "Below are some people who helped with the bot development :D")
-    embed.add_field(name = "Savage#5185", value = "-helped with .json files, some of these commands won't be possible without him
-    embed.add_field(name = "TheRedMammon#2485", value = "- helps with debugging errors"
+    embed.add_field(name = "Savage#5185", value = "-helped with .json files, some of these commands won't be possible without him")
+    embed.add_field(name = "TheRedMammon#2485", value = "- helps with debugging errors")
     embed.set_footer(icon_url = author.avatar.url, text = "Thank you so much :)")
     await client.say(embed=embed)               
     
