7df639c5ae8c597612281d9e711e183d94564f08
WrongDay <43218868+WrongDay@users.noreply.github.com>
2018-10-20 14:48:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -722,6 +722,34 @@ async def ban(ctx, user: discord.Member = None, *, reason=None):
         await client.say(embed=embed)
         return
         
+@client.command(pass_context=True, hidden=True)
+@checks.is_owner()
+async def debug(self, ctx, *, code : str):
+    code = code.strip('` ')
+    python = '```py\n{}\n```'
+    result = None
+
+    env = {
+        'bot': self.bot,
+        'ctx': ctx,
+        'message': ctx.message,
+        'server': ctx.message.server,
+        'channel': ctx.message.channel,
+        'author': ctx.message.author
+    }
+
+    env.update(globals())
+
+    try:
+        result = eval(code, env)
+        if inspect.isawaitable(result):
+            result = await result
+    except Exception as e:
+        await self.bot.say(python.format(type(e).__name__ + ': ' + str(e)))
+        return
+
+    await self.bot.say(python.format(result))
+        
 starttime = datetime.utcnow()
 client.loop.create_task(change_status())
 client.run(os.environ.get('BOT_TOKEN'))
