a074ffad91c9c00dead6b9109ac7e726f4034efd
WrongDay <43218868+WrongDay@users.noreply.github.com>
2018-10-20 16:50:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -659,16 +659,16 @@ async def kick(ctx, user: discord.Member = None, *, reason = None):
             
             if author == user:
                 embed = discord.Embed(color=0xff0200)
-                embed.set_author(icon_url=author.avatar_url, name="Uh Oh.")
-                embed.add_field(name=":x: Error:", value="```Failed to specify a user```", inline=False)
-                embed.set_footer(icon_url=author.avatar_url, text="| Moderation commands!")
+                embed.add_field(name=":x: Error: ", value="```You can't kick yourself```", inline=False)
+                embed.set_footer(icon_url=author.avatar_url, text="Error created by: {}".format(author.name))
                 await client.say(embed=embed)
                 return
             
             if user is None:
                 embed = discord.Embed(color=0xff0200)
-                embed.add_field(name=":x: Error: ", value="```You can't kick yourself```", inline=False)
-                embed.set_footer(icon_url=author.avatar_url, text="Error created by: {}".format(author.name))
+                embed.set_author(icon_url=author.avatar_url, name="Uh Oh.")
+                embed.add_field(name=":x: Error:", value="```Failed to specify a user```", inline=False)
+                embed.set_footer(icon_url=author.avatar_url, text="| Moderation commands!")
                 await client.say(embed=embed)
                 return
             
