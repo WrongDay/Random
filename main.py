c838649840b8e8b809d343b982f95813f61c73cb
WrongDay <43218868+WrongDay@users.noreply.github.com>
2018-10-20 14:52:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -729,26 +729,15 @@ async def debug(self, ctx, *, code : str):
     python = '```py\n{}\n```'
     result = None
 
-    env = {
-        'bot': self.bot,
-        'ctx': ctx,
-        'message': ctx.message,
-        'server': ctx.message.server,
-        'channel': ctx.message.channel,
-        'author': ctx.message.author
-    }
-
-    env.update(globals())
-
     try:
-        result = eval(code, env)
+        result = eval(code)
         if inspect.isawaitable(result):
             result = await result
     except Exception as e:
-        await self.bot.say(python.format(type(e).__name__ + ': ' + str(e)))
+        await client.say(python.format(type(e).__name__ + ': ' + str(e)))
         return
 
-    await self.bot.say(python.format(result))
+    await client.say(python.format(result))
         
 starttime = datetime.utcnow()
 client.loop.create_task(change_status())
