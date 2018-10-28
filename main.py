98b19b3cd1ad707a8d7c6881bb796f2722ce09f6
WrongNub <44216624+WrongNub@users.noreply.github.com>
2018-10-28 11:49:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -277,7 +277,7 @@ async def boobs(ctx):
         response = requests.get("https://nekos.life/api/v2/img/boobs")
         data = response.json()
         data = response.json()
-        embed = discord.Embed(title = "Try not to masturbate to it?", color = 0x00ff00)
+        embed = discord.Embed(title = "A man's desire? :heart_eyes:", color = 0x00ff00)
         embed.set_image(url = f"{data['url']}")
         embed.set_footer(text = f"Requested By: {ctx.message.author}")
         await client.say(embed = embed)
@@ -295,7 +295,25 @@ async def lewd(ctx):
         response = requests.get("https://nekos.life/api/v2/img/lewd")
         data = response.json()
         data = response.json()
-        embed = discord.Embed(title = "Try not to masturbate to it?", color = 0x00ff00)
+        embed = discord.Embed(title = "Are you really that desperate? :eyes:", color = 0x00ff00)
+        embed.set_image(url = f"{data['url']}")
+        embed.set_footer(text = f"Requested By: {ctx.message.author}")
+        await client.say(embed = embed)
+        
+   else:
+        embed = discord.Embed(color = 0xff0200)
+        embed.add_field(name = ":x: Stop!", value = "Reason: ```This channel isn't marked nsfw```", inline = False)
+        embed.set_footer(text='''We need to protect people's eyes :D''')
+        await client.say(embed = embed)  
+    
+@client.command(pass_context=True)
+async def anal(ctx):
+   channel_nsfw = await client.is_nsfw(ctx.message.channel)
+   if channel_nsfw:
+        response = requests.get("https://nekos.life/api/v2/img/anal")
+        data = response.json()
+        data = response.json()
+        embed = discord.Embed(title = ":peach: :eggplant:", color = 0x00ff00)
         embed.set_image(url = f"{data['url']}")
         embed.set_footer(text = f"Requested By: {ctx.message.author}")
         await client.say(embed = embed)
