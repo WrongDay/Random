d0cf77be769af2f00582220a23bb9a09fcd9bacb
autixts <43218868+autixts@users.noreply.github.com>
2020-09-11 14:56:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,34 +14,6 @@ import youtube_dl
 from datetime import datetime
 import subprocess
 from itertools import cycle 
-from discord import opus
-
-#https://pastebin.com/JMVYJpGY -reminder
-OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
-             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
-
-
-def load_opus_lib(opus_libs=OPUS_LIBS):
-    if opus.is_loaded():
-        return True
-
-    for opus_lib in opus_libs:
-            try:
-                opus.load_opus(opus_lib)
-                return
-            except OSError:
-                pass
-
-    raise RuntimeError('Could not load an opus lib. Tried %s' %
-                       (', '.join(opus_libs)))
-load_opus_lib()
-
-in_voice=[]
-
-
-players = {}
-songs = {}
-playing = {}
 
 def prefix(bot, message):
     with open("serverConfig.json") as f:
